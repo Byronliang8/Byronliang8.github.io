707b3839b566f93545f539fd523be72f23bd80a8
Byronliang8 <byronliang8@outlook.com>
2024-09-10 22:20:42 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ const IndexPage = () => {
             I am a PhD candidate in Visual Computing research section, the School of Computer Science and Informatics,
             Cardiff University. My thesis topic is <i>Uncovering the “Instincts” of Deep Generative Models for Fair and Unbiased
             Visual Content Creation</i>, supervised by Dr. Yipeng Qin, Prof. Yu-Kun Lai and Dr. Jing Wu and this project
-            is <p style="color: red;">Fully funded by Engineering and Physical Sciences Research Council (EPSRC) DTP studentships</p>.
+            is Fully funded by Engineering and Physical Sciences Research Council (EPSRC) DTP studentships.
           </p>
           <p className="mb-4">
             My research interests are centred around machine learning and its applications in computer vision, computer graphics
