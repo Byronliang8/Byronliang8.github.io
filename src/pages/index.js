d76b7f0ece5706ab1b33fc4db710115afac781b4
Byronliang8 <32118941+Byronliang8@users.noreply.github.com>
2025-06-26 09:54:37 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -233,33 +233,6 @@ const IndexPage = () => {
   )
 }
 
-export default function HTML(props) {
-  return (
-    <html {...props.htmlAttributes}>
-      <head {...props.headAttributes}>
-        {props.headComponents}
-        {/* ⬇️ Google Analytics 脚本 */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-BB4NG1TEMJ"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-BB4NG1TEMJ');
-            `,
-          }}
-        />
-      </head>
-      <body {...props.bodyAttributes}>
-        {props.preBodyComponents}
-        <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
-        {props.postBodyComponents}
-      </body>
-    </html>
-  );
-}
-
 export const Head = () => <Seo title="Home" />
 
 export default IndexPage
